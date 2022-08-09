3225a7769940c04bdb6459b0ce8ba9c156277973
Nishant Pillai <nishantpillai5@gmail.com>
2022-08-09 20:27:43 +0200

    Installation script

---
+++ run_once_install-packages.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo apt install bat tmux fzf git curl ranger tree trash-cli 
+# sudo apt install i3 scrot feh imagemagick 
+# sudo apt install neovim
+
+sudo apt install p7zip-full p7zip-rar
+
+mkdir -p ~/.local/bin
+ln -s /usr/bin/batcat ~/.local/bin/bat
