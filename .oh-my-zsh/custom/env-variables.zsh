f67d98d998bb63b163ddf1ad3f046aff223a18ac
Nishant Pillai <nishantpillai5@users.noreply.github.com>
2026-05-11 13:14:21 +0200

    shortcuts

--- .oh-my-zsh/custom/env-variables.zsh
+++
@@ -1,8 +0,0 @@
-# CUDA
-
-export CUDA_HOME=/usr/local/cuda
-export PATH="$CUDA_HOME/bin:$PATH"
-export LD_LIBRARY_PATH="$CUDA_HOME/lib64:${LD_LIBRARY_PATH:-}"
-
-# Stable diffusion UI
-export python_cmd="python3.11"
---
+++ .oh-my-zsh/custom/shortcuts.zsh
@@ -0,0 +1,13 @@
+# Dirs
+export DIR_WORK=~/work
+export DIR_NVIM=~/.config/nvim
+export DIR_ZSH=~/.oh-my-zsh/custom
+export DIR_NOTES=~/notes
+
+# Aliases
+alias ww='cd $DIR_WORK'
+alias nn='cd $DIR_NOTES'
+alias vv='cd $DIR_NVIM'
+alias zz='cd $DIR_ZSH'
+
+alias autorun='cd ~/Library/LaunchAgents'
