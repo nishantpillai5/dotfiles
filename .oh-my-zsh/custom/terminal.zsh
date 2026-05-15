3de53ccdba4fa283fedde2455c95f665ba86a722
Nishant Pillai <nishantpillai5@gmail.com>
2026-05-15 11:47:18 +0200

    sherlock instead of fuzzel

--- .oh-my-zsh/custom/terminal.zsh
+++ .oh-my-zsh/custom/terminal.zsh
@@ -4,19 +4,18 @@ export TERM=tmux-256color
 # dircolors
 PATH="$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin:$PATH"
 MANPATH="$HOMEBREW_PREFIX/opt/coreutils/libexec/gnuman:$MANPATH"
-eval `dircolors ~/.dircolors`
-test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+test -r "~/.dir_colors" && eval $(dircolors ~/.dir_colors)
 
 # Editor
 export EDITOR=nvim
- 
+
 # jk as Esc
 jk-to-vi-cmd(){
   zle vi-cmd-mode
 }
 zle -N jk-to-vi-cmd
 bindkey -M viins 'jk' jk-to-vi-cmd
- 
+
 # If this is an xterm set the title to user@host:dir
 case "$TERM" in
 xterm*|rxvt*)
